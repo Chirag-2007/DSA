dca70402c3adadbfdf98d1c4dc8aa25af7c0ba12
C-N-2007 <nagpalchirag757@gmail.com>
2026-07-01 05:34:06 +0530

    Question10

---
+++ Question10.java
@@ -0,0 +1,28 @@
+// Largest Element
+
+import java.util.Scanner;
+
+public class Question10 {
+    
+    public static int largest(int[] arr) {
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int ans = largest(arr);
+        System.out.println(ans);
+        sc.close();
+    }
+}
