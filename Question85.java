e7eaf10add099a31214f118c7d494870cfbbe40a
C-N-2007 <nagpalchirag757@gmail.com>
2026-09-14 06:01:39 +0530

    Question85

---
+++ Question85.java
@@ -0,0 +1,34 @@
+// 724. Find Pivot Index
+
+import java.util.Scanner;
+
+public class Question85 {
+
+    public static int pivotIndex(int[] arr){
+        int n = arr.length;
+        int totalSum = 0;
+        for(int i = 0; i < n; i++){
+            totalSum += arr[i];
+        }
+        int leftSum = 0;
+        for(int i = 0; i < n; i++){
+            int rightSum = totalSum - arr[i] - leftSum;
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += arr[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int ans = pivotIndex(arr);
+        System.out.println(ans);
+        sc.close();
+    }
+}
